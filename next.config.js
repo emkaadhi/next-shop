4bbd6fe398a38c9d82d1859a6487a028ddf40baf
emkaadhi <emka.adhi79@yahoo.com>
2022-07-29 21:45:38 +0700

    delete font-css

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
-/** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: false,
-//   ignoreDuringBuilds: true,
-// }
-
-// module.exports = nextConfig
-
 module.exports = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
-    webpack5: true 
   },
 }
 
